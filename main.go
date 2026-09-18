047c66297b3ed3338f8d36065999eb19446815cd
piguycs <piguycs@pm.me>
2026-09-18 11:24:08 +0200

    switch

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
+
+func sum(a, b, c int) int {
+	return a + b + c
+}
 
 func main() {
-	fmt.Printf("Hello, %s!\n", "World")
+	var os string
+	switch runtime.GOOS {
+	case "windows", "darwin":
+		os = "StupidOS"
+	case "linux":
+		os = "GoatOS"
+	default:
+		os = "hopefully TempleOS"
+	}
+
+	greeter := "World"
+	fmt.Printf("Hello %s on %s!\n", greeter, os)
+
+	for i := range 5 {
+		fmt.Printf("%d\n", i)
+	}
 }
