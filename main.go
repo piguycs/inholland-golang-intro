faf6fa4b40b8329bd2c02752e558989a9e240a11
piguycs <piguycs@pm.me>
2026-09-18 12:07:47 +0200

    practice errors

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"runtime"
 )
 
 func sum(a, b, c int) int {
 	return a + b + c
+}
+
+func dv(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("Could not divide by zero")
+	}
+	return a / b, nil
 }
 
 func main() {
@@ -25,5 +33,17 @@ func main() {
 
 	for i := range 5 {
 		fmt.Printf("%d\n", i)
+	}
+
+	if value, err := dv(1, 0); err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+	} else {
+		fmt.Printf("Div result: %d\n", value)
+	}
+
+	if value, err := dv(4, 2); err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+	} else {
+		fmt.Printf("Div result: %d\n", value)
 	}
 }
